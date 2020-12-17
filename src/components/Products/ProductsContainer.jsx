df182303ce3077951633bfd51a6a569bb3c1a27e
andresveronok <veronandresramiro@gmail.com>
2020-12-17 18:47:00 -0300

    Agregando contadores a las tarjetas

--- src/components/Products/ProductsContainer.jsx
+++ src/components/Products/ProductsContainer.jsx
@@ -5,8 +5,8 @@ import foto from '../../assets/pantalonMujer.jpg'
 function ProductsContainer() {
     return (
       <>
-        <Tarjetas imagen = {foto} titulo = "Pantalon1" descripcion = "Descripcion1" precio = "250"/>
-        <Tarjetas imagen = {foto} titulo = "Pantalon2" descripcion = "Descripcion2" precio = "300"/>        
+        <Tarjetas imagen = {foto} titulo = "Pantalon1" descripcion = "Descripcion1" precio = "250" stock="3"/>
+        <Tarjetas imagen = {foto} titulo = "Pantalon2" descripcion = "Descripcion2" precio = "300" stock="1"/>        
       </>
     );
   }
--- src/components/Tarjetas/Tarjetas.jsx
+++ src/components/Tarjetas/Tarjetas.jsx
@@ -1,19 +1,43 @@
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import {useState} from 'react'
+import './Tarjetas.css'
 
-const Tarjetas = ({imagen, titulo, descripcion, precio}) => {
+const Tarjetas = ({imagen, titulo, descripcion, precio, stock}) => {
+
+    const [cantidad, setCantidad] = useState(1);
+
+    const agregarCarrito = () => {
+        if (cantidad <= stock){
+            alert(`Agregaste ${cantidad} productos al carrito`);
+            stock = stock - cantidad
+        }else{
+            alert(`No se pudo agregar la cantidad de ${cantidad} productos al carrito porque supera al stock disponible (${stock}).`);
+        }
+    }
+
     return (
         <CardDeck>
             <Card>
-                <Card.Img variant="top" src={imagen} alt="Imagen del producto" />
+                <Card.Img class='imagen' variant="top" src={imagen} alt="Imagen del producto" />
                 <Card.Body>
                 <Card.Title>{titulo}</Card.Title>
                 <Card.Text>
                     {descripcion}
+                    <br></br>
+                    {precio}
+                    <br></br>
+                    <ButtonGroup>
+                        <Button variant="secondary" disabled={cantidad === 1 ? 'disabled' : null } onClick={() => setCantidad(cantidad - 1)}>-</Button>
+                        <h3> {cantidad} </h3>
+                        <Button variant="secondary" onClick={() => setCantidad(cantidad + 1)}>+</Button>
+                    </ButtonGroup>
                 </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                <small className="text-muted">{precio}</small>
+                <Button variant="primary" size="lg" onClick={agregarCarrito}>Agregar al carrito</Button>
                 </Card.Footer>
             </Card>
         </CardDeck>
